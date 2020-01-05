2cd5d12d65782da842851c5eed66579fb26e9b52
andwis12 <1bandrzejwisinski@gmail.com>
2020-01-05 19:56:48 +0100

    Dokumentacja

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,9 +1,23 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ *  Klasa która jest przechowywana jako element klasy IntLinkedList
+ */
+
 public class Node {
 
+	/**
+	 * value - zmienna która przechowuje wartość węzła
+	 * prev - zmienna która przechowuje referencję do poprzedniego Node'a
+	 * next - zmienna która przechowuje referencję do następnego Node'a
+	 */
 	int value;
 	public Node prev, next;
+
+	/**
+	 * Konstruktor klasy Node
+	 * @param i - zmienna która zostanie przypisana jako wartość węzła
+	 */
 
 	public Node(int i) {
 		value = i;
