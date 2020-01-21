6a46bbcc72daa729b1c5211305126d5c44f0fcf2
andwis12 <1bandrzejwisinski@gmail.com>
2020-01-21 14:48:54 +0100

    2.1

---
+++ src/main/java/edu/kis/vh/nursery/StackInterface.java
@@ -0,0 +1,19 @@
+package edu.kis.vh.nursery;
+
+public interface RhymerInterface
+{
+	void push(int i);
+
+	int pop();
+
+	boolean isFull();
+
+	boolean isEmpty();
+
+	int top();
+
+	int getTotal();
+
+
+
+}
